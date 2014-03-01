fb550c5a007758a09068a6eafd3f88bbac27e963
hgarcia <hernan@dynamicprogrammer.com>
2014-02-28 22:27:13 -0500

    [HG] Hello World.

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,14 @@
+/* globals angular */
 angular
-.module('dynamic-sports', ['ionic', 'dynamic-sports.services', 'dynamic-sports.controllers'])
-.config(function($stateProvider, $urlRouterProvider) {
+  .module('dynamic-sports', ['ionic', 'dynamic-sports.services', 'dynamic-sports.controllers'])
+  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    'use strict';
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      });
 
- $stateProvider
-    .state('home', {
-      url: "/home",
-      templateUrl: "templates/home.html",
-      controller: 'HomeCtrl'
-    });
-    // .state('about', {
-    //   url: "/about",
-    //   templateUrl: "about.html",
-    //   controller: 'AboutCtrl'
-    // })
-    // .state('contact', {
-    //   url: "/contact",
-    //   templateUrl: "contact.html"
-    // })
-
-    // if none of the above are matched, go to this one
-    $urlRouterProvider.otherwise("/home");
-});
+    $urlRouterProvider.otherwise('/home');
+  }]);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,6 @@
+/* globals angular, console */
 angular.module('dynamic-sports.controllers', [])
-.controller('HomeCtrl', function($scope) {
-  console.log('HomeCtrl');
-});
-// .controller('PetIndexCtrl', function($scope, PetService) {
-//   $scope.pets = PetService.all();
-// })
-// .controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
-//   $scope.pet = PetService.get($stateParams.petId);
-// });
+  .controller('HomeCtrl', ['$scope', function ($scope) {
+    'use strict';
+    console.log('HomeCtrl');
+  }]);
