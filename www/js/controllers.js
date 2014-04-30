f91503ac1dfcc582732587c101c2b4a52abdb1fd
hgarcia <hernan@dynamicprogrammer.com>
2014-04-30 13:38:24 -0400

    [HG] Moving to use beta.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,6 @@
-angular.module('starter.controllers', [])
-
-.controller('DashCtrl', function($scope) {
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-});
+/* globals angular, console */
+angular.module('dynamic-sports.controllers', [])
+.controller('HomeCtrl', ['$scope', function ($scope) {
+  'use strict';
+  console.log('HomeCtrl');
+}]);
